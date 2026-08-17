302399ee2b1ca090e944484305592fbdde6ec22f
HEO IK <heoikng@gmail.com>
2026-08-18 05:02:33 +0900

    Feature: Gravity, AirControl, Toggle

--- Source/SpartaKingLeonidas/Private/LeoPawn.cpp
+++ Source/SpartaKingLeonidas/Private/LeoPawn.cpp
@@ -1,35 +1,169 @@
 #include "LeoPawn.h"
-#include "Components/CapsuleComponent.h"
+#include "Components/SphereComponent.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
+#include "Components/StaticMeshComponent.h"
+
+
 
 ALeoPawn::ALeoPawn()
 {
-	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = true;
 
-	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
-	SetRootComponent(CapsuleComp);
-	CapsuleComp->SetSimulatePhysics(false);
 
-	MeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
-	MeshComp->SetupAttachment(CapsuleComp);
-	MeshComp->SetSimulatePhysics(false);
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere"));
+	SetRootComponent(SphereComp);
+	SphereComp->SetSimulatePhysics(false);
+
+	SkeltalMeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeltalMesh"));
+	SkeltalMeshComp->SetupAttachment(SphereComp);
+	SkeltalMeshComp->SetSimulatePhysics(false);
 
 	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
-	SpringArmComp->SetupAttachment(CapsuleComp);
+	SpringArmComp->SetupAttachment(SphereComp);
 	SpringArmComp->TargetArmLength = 300.f;
 	SpringArmComp->bUsePawnControlRotation = false;
 
 	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	CameraComp->SetupAttachment(SpringArmComp, USpringArmComponent::SocketName);
 	CameraComp->bUsePawnControlRotation = false;
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMesh->SetupAttachment(SphereComp);
+	StaticMesh->SetSimulatePhysics(false);
+	StaticMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
 }
 
+void ALeoPawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (APlayerController* PC = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem =
+			ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer()))
+		{
+			if (DefaultMappingContext)
+			{
+				Subsystem->AddMappingContext(DefaultMappingContext, 0);
+			}
+		}
+	}
+}
 
 void ALeoPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (MoveAction)
+		{
+			EnhancedInput->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ALeoPawn::Move);
+		}
+		if (LookAction)
+		{
+			EnhancedInput->BindAction(LookAction, ETriggerEvent::Triggered, this, &ALeoPawn::Look);
+		}
+		if (UpAction)
+		{
+			EnhancedInput->BindAction(UpAction, ETriggerEvent::Triggered, this, &ALeoPawn::Up);
+		}
+		if (TurnAction)
+		{
+			EnhancedInput->BindAction(TurnAction, ETriggerEvent::Triggered, this, &ALeoPawn::Turn);
+		}		
+		if (FlightAction)
+		{
+			EnhancedInput->BindAction(FlightAction, ETriggerEvent::Started, this, &ALeoPawn::Toggle);
+		}
+	}
 }
 
+void ALeoPawn::Move(const FInputActionValue& value)
+{
+	const FVector2D Input = value.Get<FVector2D>();
+	const float Delta = GetWorld()->GetDeltaSeconds();
+
+	if (Input.IsNearlyZero()) return;
+
+	const float Multiplier = (bFlightMode || bIsGrounded) ? 1.f : AirControlRatio;
+
+	AddActorLocalOffset(FVector(Input.X, Input.Y, 0.f) * MoveSpeed * Multiplier * Delta, true);
+}
+
+void ALeoPawn::Look(const FInputActionValue& value)
+{
+	const FVector2D Input = value.Get<FVector2D>();
+
+	AddActorLocalRotation(FRotator(0.f, Input.X, 0.f));
+	SpringArmComp->AddLocalRotation(FRotator(Input.Y, 0.f, 0.f));
+}
+
+void ALeoPawn::Up(const FInputActionValue& value)
+{
+	if (!bFlightMode) return;
+	const float Input = value.Get<float>();
+	const float Delta = GetWorld()->GetDeltaSeconds();
+
+	if (!FMath::IsNearlyZero(Input))
+	{
+		AddActorLocalOffset(FVector(0.f, 0.f, Input) * MoveSpeed * Delta, true);
+	}
+}
+
+void ALeoPawn::Turn(const FInputActionValue& value)
+{
+	const float Input = value.Get<float>();
+	const float Delta = GetWorld()->GetDeltaSeconds();
+	const float RollAmount = Input * 60.f * Delta;
+
+	AddActorLocalRotation(FRotator(0.f, 0.f, RollAmount));
+	SpringArmComp->AddLocalRotation(FRotator(0.f, 0.f, -RollAmount));
+}
+
+void ALeoPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	StaticMesh->AddLocalRotation(FRotator(0.f, SpinSpeed * DeltaTime, 0.f));
+
+	if (bFlightMode)
+	{
+		bIsGrounded = false;
+		ZVelocity = 0.f;
+		return;
+	}
+
+	FHitResult Hit;
+	const FVector Start = GetActorLocation();
+	const FVector End = Start - FVector(0.f, 0.f, GroundTraceDistance);
+
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(this);
+
+	bIsGrounded = GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECC_Visibility, Params);
+
+	if (bIsGrounded)
+	{
+		if (ZVelocity < 0.f)
+		{
+			ZVelocity = 0.f;
+		}
+	}
+	else
+	{
+		ZVelocity += Gravity * DeltaTime;
+		AddActorWorldOffset(FVector(0.f, 0.f, ZVelocity * DeltaTime), true);
+	}
+}
+void ALeoPawn::Toggle(const FInputActionValue& value)
+{
+	bFlightMode = !bFlightMode;
+	ZVelocity = 0.f;
+}
--- Source/SpartaKingLeonidas/Public/LeoPawn.h
+++ Source/SpartaKingLeonidas/Public/LeoPawn.h
@@ -5,11 +5,11 @@
 #include "InputActionValue.h"
 #include "LeoPawn.generated.h"
 
-class UCapsuleComponent;
+class USphereComponent;
 class USkeletalMeshComponent;
 class USpringArmComponent;
 class UCameraComponent;
-
+class UStaticMeshComponent;
 
 UCLASS()
 class SPARTAKINGLEONIDAS_API ALeoPawn : public APawn
@@ -24,10 +24,13 @@ public:
 
 protected:
 	UPROPERTY(VisibleAnywhere, Category = "Components")
-	UCapsuleComponent* CapsuleComp;
+	USphereComponent* SphereComp;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
-	USkeletalMeshComponent* MeshComp;
+	USkeletalMeshComponent* SkeltalMeshComp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* StaticMesh;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	USpringArmComponent* SpringArmComp;
@@ -40,4 +43,46 @@ protected:
 
 	void Move(const FInputActionValue& value);
 	void Look(const FInputActionValue& value);
+	void Up(const FInputActionValue& value);
+	void Turn(const FInputActionValue& value);
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	class UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	class UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	class UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	class UInputAction* UpAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	class UInputAction* TurnAction;
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float SpinSpeed = 30.f;
+
+	UPROPERTY(EditAnywhere, Category = "Gravity")
+	float Gravity = -980.f;
+
+	UPROPERTY(EditAnywhere, Category = "Gravity")
+	float GroundTraceDistance = 60.f;
+
+	UPROPERTY(EditAnywhere, Category = "Gravity")
+	float AirControlRatio = 0.4f;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	class UInputAction* FlightAction;
+
+	void Toggle(const FInputActionValue& value);
+
+	bool bFlightMode = true;
+	bool bIsGrounded = false;
+	float ZVelocity = 0.f;
 };
